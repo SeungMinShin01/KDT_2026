ee90c9f33151bfce0427b8fe751c2adcc8823960
SeungminShin01 <harry9096@gmail.com>
2026-07-08 17:55:14 +0900

    day03 8h

--- 2026_FE/day03/levelup.js
+++ 2026_FE/day03/levelup.js
@@ -15,25 +15,38 @@ let second = Number(prompt("초 입력"));
 let hour =
   second < 0
     ? null
-    : second < 3600
+    : second % 3600 <= 0 //0시간이하 -> 00
       ? "00"
-      : second >= 3600 && second % 3600 > 0
-        ? `${parseInt(second / 3600)}`
-        : `0${parseInt(second / 3600)}`;
+      : second % 3600 > 0 && second % 3600 < 100 && second % 3600 > 9// 1시간 이상이면서 두자리
+        ? `${parseInt(second / 3600)}` //두자리
+        : `0${parseInt(second / 3600)}`; //한자리
 
-// let miniute =
-//   second < 0
-//     ? null
-//     : second < 60
-//       ? "00"
-//       : second >= 60 && second % 60 > 0 ?
-//     //   second <= 3540 && second / 60 > 9
+/* 
+     second % 3600 > 0 && second % 3600 > 9// 1시간 이상이면서 두자리
+     
+     ## 99시간 이하여야한다. + 
+*/
+let miniute =
+  second < 0
+    ? null
+    : second % 360 == 0 ? "00" //0분이하
+    : second % 360 > 0 && second % 360 < 60   //1분이상 59분 이하
+    ?  
+      
+    //   second <= 3540 && second / 60 > 9
 //     ? `${parseFloat(second / 60)}`
 //     : `0{parseFloat(second/60`;
 /* 
-    60 미만은 00 ,
-    60 이상 360 이하.
-    360 이상?? 예: 3660 = 01:01:00
+    1분이상 59분이하
+    60분이상 => 1시간
+    3661 % 60 61 
+    61 - 60 = 1
+    3600 % 60 - 60 = 0
+    36060 % 60 - 60 = 541?
+    3660 % 60 % 60
+    36061 % 360 =61
+    (second % 360 ) - Parseint(second % 360) 
+    -> 나머지 - 시간부분?
 */
 
 // let second2 =
