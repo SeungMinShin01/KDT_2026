e6a15998f93431bfd439ca72e1ab184dc56b897c
SeungminShin01 <harry9096@gmail.com>
2026-07-08 18:23:11 +0900

    levelUp
    시간 계산식 다시 처음부터 설계 필요

--- 2026_FE/day03/levelup.js
+++ 2026_FE/day03/levelup.js
@@ -15,27 +15,38 @@ let second = Number(prompt("초 입력"));
 let hour =
   second < 0
     ? null
-    : second % 3600 <= 0 //0시간이하 -> 00
+    : parseInt(second / 3600) < 0 //0시간이하 -> 00
       ? "00"
-      : second % 3600 > 0 && second % 3600 < 100 && second % 3600 > 9// 1시간 이상이면서 두자리
-        ? `${parseInt(second / 3600)}` //두자리
-        : `0${parseInt(second / 3600)}`; //한자리
+      : parseInt(second / 3600) >= 100 // 100시간 이상
+        ? null
+        : parseInt(second / 3600) > 9 // 1시간 이상이면서 두자리
+          ? `${parseInt(second / 3600)}` //두자리
+          : `0${parseInt(second / 3600)}`; //한자리
 
 /* 
-     second % 3600 > 0 && second % 3600 > 9// 1시간 이상이면서 두자리
-     
+    second /60 (분) /60 (시) 에서 ParseInt로 시간 부분만 
+    
      ## 99시간 이하여야한다. + 
 */
-let miniute =
+let minute =
   second < 0
     ? null
-    : second % 360 == 0 ? "00" //0분이하
-    : second % 360 > 0 && second % 360 < 60   //1분이상 59분 이하
-    ?  
-      
-    //   second <= 3540 && second / 60 > 9
-//     ? `${parseFloat(second / 60)}`
-//     : `0{parseFloat(second/60`;
+    : parseInt((second % 3600) % 60) <= 0
+      ? "00" //0분이하
+      : parseInt((second % 3600) % 60) > 9
+        ? `${parseInt((second % 3600) % 60)}`
+        : `0${parseInt((second % 3600) % 60)}`;
+
+let second2 =
+  second < 0
+    ? null
+    : parseInt(second % 60) % 60 <= 0
+      ? "00"
+      : parseInt(second % 60) % 60 > 9
+        ? `${parseInt(second % 60) % 60}`
+        : `0${parseInt(second % 60) % 60}`;
+
+console.log(`${hour}:${minute}:${second2}`);
 /* 
     1분이상 59분이하
     60분이상 => 1시간
@@ -44,9 +55,9 @@ let miniute =
     3600 % 60 - 60 = 0
     36060 % 60 - 60 = 541?
     3660 % 60 % 60
-    36061 % 360 =61
-    (second % 360 ) - Parseint(second % 360) 
-    -> 나머지 - 시간부분?
+
+
+
 */
 
 // let second2 =
@@ -56,7 +67,5 @@ let miniute =
 //       ? second
 //       : `0${second}`;
 // $;
-
-console.log(`${hour}:00:00`);
 
 // :${miniute}:${second}
