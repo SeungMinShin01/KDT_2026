50dbac5cb454adbc8315a9d1bebd12c2a0315f25
SeungminShin01 <harry9096@gmail.com>
2026-08-20 11:23:31 +0900

    day 28 2h

--- 2026B_BE/src/day13/exam/exam4.java
+++ 2026B_BE/src/day13/exam/exam4.java
@@ -18,8 +18,7 @@ public class exam4 {
         boolean value4 = random.nextBoolean();
         System.out.println(value4); // true / false 난수
 
-        // [2] UUID , 범용 고유 식별자 , 절대 중복없는 난수 생성 , 활용처: 회원번호
-
+        // [2] UUID , 범용 고유 식별자 , 절대 중복없는 난수 생성 , 활용처: 회원번호/파일식별
         String uuid = UUID.randomUUID().toString();
         System.out.println(uuid); // 91ce0f1e-7a0a-4034-be44-fd94d3d3b07e
 
