f14a64d7c0e28c215ddfd50ac9620489074f7d84
SeungminShin01 <harry9096@gmail.com>
2026-09-14 10:20:51 +0900

    day40 1h

---
+++ 2026_React/src/example/day02/pracitce1.jsx
@@ -0,0 +1,44 @@
+// REACT Practice1 : Practice1 과 Profile 컴포넌트를 구현하여 그림과 같이 완성하시오.
+// AXIOS 이용하여 서버로 부터 받은 데이터/자료 가정
+export default function Practice1(props) {
+  const data = [
+    { name: "Hedy Lamarr", imageUrl: "https://i.pravatar.cc/150?img=47" },
+    { name: "Grace Hopper", imageUrl: "https://i.pravatar.cc/150?img=48" },
+    { name: "Ada Lovelace", imageUrl: "https://i.pravatar.cc/150?img=49" },
+    { name: "Margaret Hamilton", imageUrl: "https://i.pravatar.cc/150?img=50" },
+  ];
+
+  return (
+    <>
+      <Profile name={data[0].name} imageUrl={imageUrl}></Profile>
+      <Profile name={data[1].name} imageUrl={imageUrl}></Profile>
+      {/* JSX 주석 */}
+      {data.map((i) => {
+        return (
+          <>
+            <Profile name={i.name} imageUrl={i.imageUrl} />
+          </>
+        );
+      })}
+    </>
+  );
+} // func end
+
+/*
+ {data.map((i) => {
+        return (
+          <>
+            <Profile name={i.name} imageUrl={i.imageUrl} />
+          </>
+        );
+      })}
+*/
+
+function Profile(props) {
+  return (
+    <>
+      <h3>{props.name}</h3>
+      <img src={props.imageUrl} />
+    </>
+  );
+} // func end
