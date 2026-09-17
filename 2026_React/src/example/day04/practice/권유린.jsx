90f846d0f9982d28f3135b734300cb0349d31295
SeungminShin01 <harry9096@gmail.com>
2026-09-17 14:09:13 +0900

    day42 4h

--- 2026_React/src/example/day04/practice/권유린.jsx
+++ 2026_React/src/example/day04/practice/권유린.jsx
@@ -1,8 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Practice2(props) {
+  const [myJSON, setMyJSON] = useState([]);
+  useEffect(async function () {
+    const response = await axios.get(
+      "https://discussions-louis-bradley-blue.trycloudflare.com/api/categories",
+    );
+    const data = response.data;
+    console.log(data);
+    setMyJSON(data); // 통신 응답결과 상태변수에 대입 --> 렌더링
+  }, []);
+
+  let trTag = myJSON.map((data) => {
+    return (
+      <tr>
+        <td>{data.cno}</td>
+        <td>{data.name}</td>
+      </tr>
+    );
+  });
+
   return (
     <>
       <div className="Box">
-        <h2>권유린</h2>
+        <h2>권유린 [카테고리 전체 조회] </h2>
 
         <div>
           <span>학과:</span>
@@ -13,6 +35,19 @@ export default function Practice2(props) {
         <span>자기소개:</span>
 
         <span>안녕하세용</span>
+      </div>
+
+      <span>카테고리 목록:</span>
+      <div>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>카테고리번호</th>
+              <th>카테고리명</th>
+            </tr>
+          </thead>
+          <tbody>{trTag}</tbody>
+        </table>
       </div>
     </>
   );
